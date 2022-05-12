845278850cb5bb551849f60564bacfcad33aeeb1
Ryan Milton <rmilton@ucla.edu>
2022-05-12 13:27:47 -0700

    Fixing directory

---
+++ build/CMakeFiles/Components_athena.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Components_athena"
+  "libathena.components"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Components_athena.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
