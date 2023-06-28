ea197e23c760294ebcca5c1baa00bc9243b4eb7a
rymilton <rmilton@ucla.edu>
2023-06-28 16:17:23 -0700

    Added an XML file option

--- insert_setup.sh
+++ insert_setup.sh
@@ -2,3 +2,4 @@
 
 export DETECTOR_PATH="${EIC_SHELL_PREFIX}/share/eic_endcap_insert"
 export SCRIPTS_PATH="${DETECTOR_PATH}/scripts"
+export DETECTOR="endcapP_insert" # Name used for XML file you want to use
