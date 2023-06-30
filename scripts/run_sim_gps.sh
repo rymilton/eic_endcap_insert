63701549405a70ece2fd55a7f08d82eebc084735
rymilton <rmilton@ucla.edu>
2023-06-30 10:21:40 -0700

    Added options for theta min and theta max Made reconstruction script depend on DETECTOR variable

--- scripts/run_sim_gps.sh
+++ scripts/run_sim_gps.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 function print_the_help {
-  echo "USAGE: ${0} -n <nevents> -part <particle> -p <momentum> "
+  echo "USAGE: ${0} -n <nevents> -part <particle> -p <momentum> -thmin <theta_min> -thmax <theta_max>"
   echo "  OPTIONS: "
   echo "    -n,--nevents     Number of events"
   echo "    -part,--particle particle type"
   echo "    -p, --momentum   particle momentum (GeV)"
+  echo "    -thmin, --theta_min   Minimum theta (degrees)"
+  echo "    -thmax, --theta_max   Maximum theta (degrees)" 
+  echo "    theta (polar angle) will generate uniformly between theta_min and theta_max"
   exit
 }
 
@@ -30,6 +33,12 @@ elif [ "$1" = "-n" -o "$1" = "--nevents" ]; then
    shift 2 # past argument
 elif [ "$1" = "-p" -o "$1" = "--momentum" ]; then
    beam_energy=$2
+   shift 2 # past argument
+elif [ "$1" = "-thmin" -o "$1" = "--theta_min" ]; then
+   theta_min=$2
+   shift 2 # past argument
+elif [ "$1" = "-thmax" -o "$1" = "--theta_max" ]; then
+   theta_max=$2
    shift 2 # past argument
 else
    break
@@ -59,4 +68,4 @@ npsim  --runType run  --enableG4GPS \
 
 # Running reconstruction
 export JUGGLER_SIM_FILE=${simfile} JUGGLER_REC_FILE=${recofile} JUGGLER_N_EVENTS=${num_events}
-gaudirun.py ${SCRIPTS_PATH}/endcapP_insert_reco.py
+gaudirun.py ${SCRIPTS_PATH}/${DETECTOR}_reco.py
